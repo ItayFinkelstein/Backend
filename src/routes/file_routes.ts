d861b4fcf58f556ba69d9f2c3d83cb55d4d43d9b
Gil <gilgil.abramovich@gmail.com>
2025-03-20 00:30:50 +0200

    Add multer to package.json

--- src/routes/file_routes.ts
+++ src/routes/file_routes.ts
@@ -6,24 +6,24 @@ const router = express.Router();
 
 const baseUrl = process.env.DOMAIN_BASE || "http://localhost:3000";
 const storage: StorageEngine = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'storage/');
-    },
-    filename: (req, file, cb) => {
-        const fileExtension = path.extname(file.originalname);
-        cb(null, `${Date.now()}${fileExtension}`);
-    }
+  destination: (req, file, cb) => {
+    cb(null, "storage/");
+  },
+  filename: (req, file, cb) => {
+    const fileExtension = path.extname(file.originalname);
+    cb(null, `${Date.now()}${fileExtension}`);
+  },
 });
 
 const upload = multer({ storage });
 
-router.post('/', upload.single("file"), (req: Request, res: Response): void => {
-    if (!req.file) {
-        res.status(400).send({ error: "File upload failed" });
-        return;
-    }
-    const fileUrl = `${baseUrl}/storage/${req.file.filename}`;
-    res.status(200).send({ url: fileUrl });
+router.post("/", upload.single("file"), (req: Request, res: Response): void => {
+  if (!req.file) {
+    res.status(400).send({ error: "File upload failed" });
+    return;
+  }
+  const fileUrl = `${baseUrl}/storage/${req.file.filename}`;
+  res.status(200).send({ url: fileUrl });
 });
 
 export default router;
