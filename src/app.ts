20cd361edaa0e88ff9bc66f7ba1394689441ad2d
Itay <itay.finkelstein@gmail.com>
2025-03-19 20:12:15 +0200

    deployment configs

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,25 @@
 import initApp from './server';
 import { Express } from 'express';
+import https from "https"
+import fs from "fs"
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const runApp = async () => {
     try {
         const app: Express = await initApp();
-        app.listen(port, () => {
-            console.log(`Post and comment app listening to port ${port}`);
-        });
+        if (process.env.NODE_ENV != "production") {
+            app.listen(port, () => {
+                console.log(`Example app listening at http://localhost:${port}`);
+            });
+        } else {
+            const prop = {
+                key: fs.readFileSync("/home/st111/Backend/client-key.pem"),
+                cert: fs.readFileSync("/home/st111/Backend/client-cert.pem")
+            }
+            console.log(port);
+            https.createServer(prop, app).listen(port)
+        }
     } catch (error) {
         console.error("Failed to initialize the app: ", error);
     }
