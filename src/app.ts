3d6c1654186664e3574aa82a7e1cf31cfa63b304
Itay <itay.finkelstein@gmail.com>
2025-03-20 23:44:29 +0200

    deployment changes

--- src/app.ts
+++ src/app.ts
@@ -2,23 +2,28 @@ import initApp from './server';
 import { Express } from 'express';
 import https from "https"
 import fs from "fs"
+import path from "path";
 
 const port = process.env.PORT || 4000;
+const domain = process.env.DOMAIN_BASE || "localhost";
 
 const runApp = async () => {
     try {
         const app: Express = await initApp();
         if (process.env.NODE_ENV != "production") {
             app.listen(port, () => {
-                console.log(`Example app listening at http://localhost:${port}`);
+                console.log(`Food app listening at http://localhost:${port}`);
             });
         } else {
+            const keyPath = path.resolve(__dirname, '../../client-key.pem');
+            const certPath = path.resolve(__dirname, '../../client-cert.pem');
             const prop = {
-                key: fs.readFileSync("/home/st111/Backend/client-key.pem"),
-                cert: fs.readFileSync("/home/st111/Backend/client-cert.pem")
-            }
-            console.log(port);
-            https.createServer(prop, app).listen(port)
+                key: fs.readFileSync(keyPath),
+                cert: fs.readFileSync(certPath)
+            };
+            https.createServer(prop, app).listen(port, () => {
+                console.log(`Food app listening at https://${domain}:${port}`);
+            });
         }
     } catch (error) {
         console.error("Failed to initialize the app: ", error);
