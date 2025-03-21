6da80982155d93233dc208984275d0a6c6f3669d
Gil <gilgil.abramovich@gmail.com>
2025-03-22 00:29:30 +0200

    minor

--- src/controllers/post_controller.ts
+++ src/controllers/post_controller.ts
@@ -36,9 +36,7 @@ class PostController extends BaseController<typeof postModel> {
       if (page !== undefined && page !== null) {
         const limit = parseInt(process.env.LIMIT_POST_FETCHING || "10", 10);
         const skip = (page - 1) * limit;
-        console.log("skip", skip);
         posts = await postModel.find(filter).skip(skip).limit(limit);
-        console.log(posts);
         const totalPostsAmount = await postModel.countDocuments(filter);
 
         res.status(200).send({
